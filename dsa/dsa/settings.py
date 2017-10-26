56da239e2b451ed25f44168e871de46f9c20203a
AleksandrTan <rumych@list.ru>
2017-10-26 12:27:37 +0300

    ferst1

--- dsa/dsa/settings.py
+++ dsa/dsa/settings.py
@@ -84,6 +84,14 @@ DATABASES = {
         'NAME': 'dsadb',
         'USER': 'root',
         'PASSWORD': 'root',
+    },
+    'dsdb': {
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'dsdb',
+        'USER': 'root',
+        'PASSWORD': 'root',
     }
 }
 
